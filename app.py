e81622997ab6857e3e1f20b8251cd07708c159c8
RahulNavneeth <rahulmnavneeth@gmail.com>
2024-12-18 11:40:31 +0530

    [migrate] cors

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from routes import blueprint
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
+
 app.register_blueprint(blueprint)
 
+cors = CORS(
+    app, origins=["chrome-extension://ljdpmpahkhiplmmchgjekbcdbimidlnk"])
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=6969)
--- routes.py
+++ routes.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, json, jsonify, request
-# from ai import VectorDB
+from flask import Blueprint, jsonify, request
+from flask_cors import CORS
+
+from utils.parseHtml import parse_html
 
 blueprint = Blueprint('routes', __name__)
+CORS(blueprint, origins=[
+     "chrome-extension://ljdpmpahkhiplmmchgjekbcdbimidlnk"])
 
 
 @blueprint.route('/', methods=['GET'])
 def test():
     return jsonify({'message': 'Hello, World!'})
 
+
+@blueprint.route('/parse', methods=['POST'])
+def parseHtml():
+    data = request.get_json()
+    # os.system(f"echo {parse_html(data["html"])} > parse.out")
+    return jsonify({'message': 'parse route!!!'})
 
 # @blueprint.route('/ingest', methods=['POST'])
 # def ingest_data(data):
